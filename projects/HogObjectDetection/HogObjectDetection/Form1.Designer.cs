bd6f03d7ae2a62d77faca79bac7231a641965758
birolkuyumcu <bluekid70@gmail.com>
2017-12-09 10:10:29 +0300

    Hog Object Detection project aded

---
+++ projects/HogObjectDetection/HogObjectDetection/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace HogObjectDetection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Video File :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(79, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(315, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(400, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(17, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(480, 240);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(17, 298);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(480, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(442, 14);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(108, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "People Detection";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 355);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Hog Object Detection";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ projects/HogObjectDetection/HogObjectDetection/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+
+namespace HogObjectDetection
+{
+    public partial class Form1 : Form
+    {
+        CvCapture cap;
+        HOGDescriptor hog;      
+        bool Playing = false;
+        IplImage src;
+
+        public Form1()
+        {
+            InitializeComponent();
+            hog = new HOGDescriptor();
+            hog.SetSVMDetector(HOGDescriptor.GetDefaultPeopleDetector());  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.FileName = "*.avi";
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                cap = CvCapture.FromFile(op.FileName);
+                Playing = true;
+                timer1.Enabled = true;
+                timer1.Interval = (int)(1000.0 / cap.GetCaptureProperty(CaptureProperty.Fps));
+                trackBar1.Maximum = (int)cap.GetCaptureProperty(CaptureProperty.FrameCount);
+                textBox1.Text = op.FileName;
+
+
+
+            }
+
+
+        }
+
+        private void HOGDrawObjects()
+        {
+            Mat img = new Mat(src, true);
+            CvRect[] found = hog.DetectMultiScale(img, 0, new CvSize(8, 8), new CvSize(24, 16), 1.05, 2);
+            foreach (CvRect rect in found)
+            {
+                // the HOG detector returns slightly larger rectangles than the real objects.
+                // so we slightly shrink the rectangles to get a nicer output.
+                CvRect r = new CvRect
+                {
+                    X = rect.X + (int)Math.Round(rect.Width * 0.1),
+                    Y = rect.Y + (int)Math.Round(rect.Height * 0.1),
+                    Width = (int)Math.Round(rect.Width * 0.8),
+                    Height = (int)Math.Round(rect.Height * 0.8)
+                };
+                img.Rectangle(r.TopLeft, r.BottomRight, CvColor.Red, 3);
+            }
+            src = img.ToIplImage();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Playing == false) return;
+            src = cap.QueryFrame();
+            if (src == null)
+            {
+                Playing = false;
+                return;
+            }
+            if(checkBox1.Checked )
+            HOGDrawObjects();
+            int pos = (int) cap.GetCaptureProperty(CaptureProperty.PosFrames);
+            if ((pos + 1) == trackBar1.Maximum)
+            {
+                Playing = false;
+
+            }
+
+            trackBar1.Value = pos;
+            pictureBox1.Image = src.ToBitmap();
+        }
+
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            if (Playing == false) return;
+            Playing = false;
+            cap.SetCaptureProperty(CaptureProperty.PosFrames, (double)trackBar1.Value);
+            Playing = true;            
+        }
+
+    }
+}
